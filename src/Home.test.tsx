6136f01baa9d146450a49d4e2b9f1d58bd6b0bc6
lkarpenko <lkarpenko@alteryx.com>
2021-07-04 16:09:49 +0800

    change

--- src/Home.test.tsx
+++ src/Home.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Home from './App';
+import Home from './Home';
 
 test('renders learn react link', () => {
   render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home Page!/i);
   expect(linkElement).toBeInTheDocument();
 });
